98263bbef5d8c6f28381860d34ff446d60543ba3
Endre Vegh <endre.vegh@yahoo.com>
2018-12-25 22:17:55 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+const Todos = props => {
+  return props.todos.map(todo => (
+    <div
+      key={todo.id}
+      style={{ display: 'flex', justifyContent: 'space-around' }}
+    >
+      <div style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+        {todo.name}
+      </div>
+      <button onClick={() => props.toggle(todo)}>
+        {todo.completed ? `Mark it undone` : `Mark it complete`}
+      </button>
+    </div>
+  ));
+};
+
+class CreateTodo extends Component {
+  state = {
+    input: ''
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-around',
+            flexDirection: 'column'
+          }}
+        >
+          <input
+            value={this.state.input}
+            onChange={e => this.setState({ input: e.target.value })}
+          />
+          <button
+            onClick={() =>
+              this.props.onTodoCreation({
+                name: this.state.input,
+                completed: false,
+                id: Math.round(Math.random() * 10000, 2)
+              })
+            }
           >
-            Learn React
-          </a>
-        </header>
+            Create todo
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        completed: false,
+        id: 1,
+        name: 'Where did I go wrong?'
+      },
+      {
+        completed: false,
+        id: 2,
+        name: 'Make coffee!'
+      },
+      {
+        completed: false,
+        id: 3,
+        name: 'Clean the house'
+      }
+    ]
+  };
+
+  handleToggle = todo => {
+    const copy = [...this.state.todos];
+
+    const index = copy.findIndex(t => t.id === todo.id);
+
+    copy[index] = { ...copy[index], completed: !copy[index].completed };
+
+    this.setState({ todos: copy });
+  };
+
+  handleTodoAppend = todo => {
+    this.setState(state => ({ todos: [...state.todos, todo] }));
+  };
+
+  render() {
+    return (
+      <div>
+        <Todos todos={this.state.todos} toggle={this.handleToggle} />
+        <CreateTodo onTodoCreation={this.handleTodoAppend} />
       </div>
     );
   }
